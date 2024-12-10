dd975c0d4b99b6dd475c9b345d7df3312e45aebb
dani10x <alzatedaniel07@gmail.com>
2024-12-10 16:14:59 -0500

    feat: :tada: Buscaminas
    Se inica proyecto de buscaminas

---
+++ src/test/java/com/pdyp/minesweeper/MinesweeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdyp.minesweeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinesweeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
