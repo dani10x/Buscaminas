5c2fa7ef61a3c6dc8a106a9a3b336a94b8613f7b
dani10x <alzatedaniel07@gmail.com>
2024-12-16 14:46:01 -0500

    fix: :wrench: Cors
    Se añade configuracion para cors

--- src/main/java/com/pdyp/minesweeper/config/WebSocketConfig.java
+++ src/main/java/com/pdyp/minesweeper/config/WebSocketConfig.java
@@ -2,12 +2,7 @@ package com.pdyp.minesweeper.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -30,6 +25,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/buscaminas")
+                .setAllowedOrigins("*")
                 .addInterceptors(webSocketSessionInterceptor);
     }
 }
